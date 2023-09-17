f5129fb4497016c4bb60c62a10c84e2de33441a0
BonhaengLee <bon952014@ajou.ac.kr>
2023-09-17 16:01:42 +0900

    Chore: 자잘한 변경

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,16 +6,16 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "docu-notion sample site",
+  title: "Web Developer Study Notes",
   tagline: "",
   url: "https://sillsdev.github.io",
-  baseUrl: "/docu-notion-sample-site/",
+  baseUrl: "/web-developer-study-notes/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
-  organizationName: "sillsdev", // Usually your GitHub org/user name.
-  projectName: "docu-notion-sample-site", // Usually your repo name.
+  organizationName: "leebonhaeng", // Usually your GitHub org/user name.
+  projectName: "web-developer-study-notes", // Usually your repo name.
   deploymentBranch: "gh-pages",
 
   presets: [
@@ -41,15 +41,21 @@ const config = {
     ],
   ],
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "fr"],
+    defaultLocale: "ko",
+    locales: ["ko"],
     localeConfigs: {
-      en: {
-        label: "English",
+      ko: {
+        label: "Korean",
         direction: "ltr",
-        htmlLang: "en-US",
+        htmlLang: "ko-KR",
         calendar: "gregory",
       },
+      // en: {
+      //   label: "English",
+      //   direction: "ltr",
+      //   htmlLang: "en-US",
+      //   calendar: "gregory",
+      // },
     },
   },
   themes: [
@@ -83,7 +89,7 @@ const config = {
         ignorePatterns: [],
       },
       navbar: {
-        title: "Sample Docusaurus Site using docu-notion",
+        title: "Web Developer Study Notes",
         logo: {
           alt: "Logo",
           src: "img/logo.svg",
